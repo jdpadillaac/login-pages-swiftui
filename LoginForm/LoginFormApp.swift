9b924ce8140253ea5948c9519a80661b82bcb26a
Jonatan Padilla <jonatan.padilla@soytul.com>
2023-06-15 15:13:52 -0500

    Initial Commit

---
+++ LoginForm/LoginFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormApp.swift
+//  LoginForm
+//
+//  Created by Jonatan Padilla on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
